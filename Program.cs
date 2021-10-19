05245196ce893a0742beb17ddbe7fd5c3c3861f9
Opilane TTHK <opilane@tthk.ee>
2021-10-19 11:39:26 +0300

    s

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.Write("Sisesta esimene number : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Sisesta teine number : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vali tehe ('+', '-', '*' või '/'");
+            char userOperation = Convert.ToChar(Console.ReadLine());
+
+            switch (userOperation)
+            {
+                case '+':
+                    AddTwoNumbers(num1, num2);
+                    break;
+                case '/':
+                    DivideTwoNumbers(num1, num2);
+                    break;
+                case '*':
+                    MultiplyTwoNumbers(num1, num2);
+                    break;
+                    
+
+
+
+
+                   
+
+            
+            }
+            
+
+
+            {
+
+
+            }
+
+
+
+
+        }
+
+        private static void AddTwoNumbers(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine($"{x} + {y} = {result} ");
+
+        }
+
+        private static void DivideTwoNumbers(int x, int y)
+        {
+            int result = x / y;
+            Console.WriteLine($" {x} / {y} = {result}. ");
+
+        }
+
+        private static void MultiplyTwoNumbers(int x, int y)
+        {
+            int result = x * y;
+            Console.WriteLine($" {x} * {y} = {result}.");
+
+        }
+
+    }
+}
